7570c7e60c636f9fb5935a2f822fb9d912c01014
saj panchal <sajpanchal2020@outlook.com>
2021-09-12 10:33:40 -0400

    project initiated.

---
+++ Shared/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  Shared
+//
+//  Created by saj panchal on 2021-09-12.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
