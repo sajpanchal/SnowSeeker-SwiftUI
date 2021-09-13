1a7b99a3a65684982b8934825025095e332958da
saj panchal <sajpanchal2020@outlook.com>
2021-09-12 21:02:30 -0400

    building primary list of items.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,33 +6,32 @@
 //
 
 import SwiftUI
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna, Arya and Toby")
-        }
-    }
-}
+
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
     var body: some View {
-        Group {
-            if sizeClass == .compact {
-                VStack {
-                    UserView()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: Text(resort.name)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.black, lineWidth: 1))
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
-            else {
-                HStack {
-                    UserView()
-                }
-            }
+            .navigationBarTitle("Resorts")
         }
-        
-        
-       
     }
 }
 
